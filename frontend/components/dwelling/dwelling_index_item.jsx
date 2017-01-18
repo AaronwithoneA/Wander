a415e39f570de5fd4b5b5f9b8d6003ef7f5f5c3e
Aron <zachc@Zachs-MacBook-Air-3.local>
2017-01-18 13:41:55 -0800

    Complete dwelling search index formatting

--- frontend/components/dwelling/dwelling_index_item.jsx
+++ frontend/components/dwelling/dwelling_index_item.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {hashHistory} from 'react-router';
 
 class DwellingIndexItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.imageLink = this.imageLink.bind(this);
+  }
+
+  imageLink (e) {
+    e.preventDefault;
+    hashHistory.push(`/dwellings/${this.props.dwelling.id}`);
+  }
+
 
   render () {
     return (
       <li className="dwelling-box">
-          <img className="dwelling-index-image" src={this.props.dwelling.image_url}></img>
+            <img
+              className="dwelling-index-image"
+              src={this.props.dwelling.image_url}
+              onClick={this.imageLink}/>
+
+
+
           <div className="dwelling-index-info">
             <div className="dwelling-index-price-location-box"></div>
             <div className="dwelling-index-price">{this.props.dwelling.price}</div>
